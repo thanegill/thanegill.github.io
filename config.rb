7195c7cb6c3ec1f8a37b052ca5ccb00a88c054d4
Thane Gill <me@thanegill.com>
2015-12-21 11:39:42 -0800

    Basic middleman-blog setup

--- config.rb
+++ config.rb
@@ -2,11 +2,11 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
+Time.zone = "America/Los_Angeles"
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blog"
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
@@ -18,7 +18,7 @@ activate :blog do |blog|
   # blog.year_link = "{year}.html"
   # blog.month_link = "{year}/{month}.html"
   # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
+  blog.default_extension = ".md"
 
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
@@ -27,6 +27,10 @@ activate :blog do |blog|
   # blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
+
+  # Path (relative to project root) to an ERb template that will be used to
+  # generate new articles from the "middleman article" command.
+  blog.new_article_template = "article_template.md.erb"
 end
 
 page "/feed.xml", layout: false
@@ -65,11 +69,13 @@ page "/feed.xml", layout: false
 # Helpers
 ###
 
+activate :directory_indexes
+
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -79,24 +85,22 @@ page "/feed.xml", layout: false
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
