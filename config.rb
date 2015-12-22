0735dc8838b75e2cd9316c139a61d4d751f3aea6
Thane Gill <me@thanegill.com>
2015-12-21 23:22:52 -0800

    Enable blog config options

--- config.rb
+++ config.rb
@@ -23,20 +23,23 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = 'blog'
 
-  # blog.permalink = '{year}/{month}/{day}/{title}.html'
   # Matcher for blog source files
-  # blog.sources = '{year}-{month}-{day}-{title}.html'
-  # blog.taglink = 'tags/{tag}.html'
-  blog.layout = 'article'
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = '{year}.html'
-  # blog.month_link = '{year}/{month}.html'
-  # blog.day_link = '{year}/{month}/{day}.html'
+  blog.sources = '{year}/{month}-{day}-{title}.html'
   blog.default_extension = '.md'
 
+  blog.permalink = '{year}/{month}/{day}/{title}.html'
+  blog.layout = 'article'
+
   blog.tag_template = 'tag.html'
+  blog.taglink = 'tags/{tag}.html'
+
   blog.calendar_template = 'calendar.html'
+  blog.year_link = '{year}.html'
+  blog.month_link = '{year}/{month}.html'
+  blog.day_link = '{year}/{month}/{day}.html'
+
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
 
   # Enable pagination
   # blog.paginate = true
