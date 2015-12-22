a461953cf7ac18727fc1b3f75aa00b6f4881b052
Thane Gill <me@thanegill.com>
2015-12-21 21:13:02 -0800

    Use single quotes

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 # Blog settings
 ###
 
-Time.zone = "America/Los_Angeles"
+Time.zone = 'America/Los_Angeles'
 
 
 set :haml, { :ugly => true, :format => :html5 }
@@ -21,34 +21,34 @@ set :markdown,
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  blog.prefix = "blog"
+  blog.prefix = 'blog'
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # blog.permalink = '{year}/{month}/{day}/{title}.html'
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
-  # blog.taglink = "tags/{tag}.html"
-  blog.layout = "article"
+  # blog.sources = '{year}-{month}-{day}-{title}.html'
+  # blog.taglink = 'tags/{tag}.html'
+  blog.layout = 'article'
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  blog.default_extension = ".md"
+  # blog.year_link = '{year}.html'
+  # blog.month_link = '{year}/{month}.html'
+  # blog.day_link = '{year}/{month}/{day}.html'
+  blog.default_extension = '.md'
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  blog.tag_template = 'tag.html'
+  blog.calendar_template = 'calendar.html'
 
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
-  # blog.page_link = "page/{num}"
+  # blog.page_link = 'page/{num}'
 
   # Path (relative to project root) to an ERb template that will be used to
-  # generate new articles from the "middleman article" command.
-  blog.new_article_template = "article_template.md"
+  # generate new articles from the 'middleman article' command.
+  blog.new_article_template = 'article_template.md'
 end
 
-page "/feed.xml", layout: false
+page '/feed.xml', layout: false
 
 ###
 # Compass
@@ -66,19 +66,19 @@ page "/feed.xml", layout: false
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", layout: false
+# page '/path/to/file.html', layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# page '/path/to/file.html', layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
-#   page "/admin/*"
+#   page '/admin/*'
 # end
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
+#  which_fake_page: 'Rendering a fake page with a local variable' }
 
 ###
 # Helpers
@@ -95,7 +95,7 @@ activate :livereload
 # Methods defined in the helpers block are available in templates
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
@@ -119,5 +119,5 @@ configure :build do
   activate :relative_assets
 
   # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  # set :http_prefix, '/Content/images/'
 end
